f6eefa3eba39e16c947b268d31b863b1dd03ac3d
Ramón López Castiñeira <84033537+ramoncasti@users.noreply.github.com>
2024-05-22 22:09:05 -0400

    Se agrego Entity Framewokr

---
+++ Repository/Contexts/ContextoAplicacionDB.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Contexts
+{
+    public class ContextoAplicacionDB : DbContext
+    {
+        public DbSet<ClienteModel> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ClienteModel>().HasKey(p=>p.Id);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public ContextoAplicacionDB(DbContextOptions<ContextoAplicacionDB> options) : base(options) { 
+            
+        }
+    }
+}
--- WebApiParcial1/Program.cs
+++ WebApiParcial1/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Repository.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connectionString = builder.Configuration.GetConnectionString("postgresConnection");
+
+builder.Services.AddDbContext<ContextoAplicacionDB>(options =>
+{
+    options.UseNpgsql(connectionString);
+}
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
